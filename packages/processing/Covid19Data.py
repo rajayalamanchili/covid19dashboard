7829b122d54f4b552921ba403bb748969adbf48d
rajayalamanchili <raja.yalamanchili2@gmail.com>
2020-04-06 09:28:17 -0400

    added covid processing module

---
+++ packages/processing/Covid19Data.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr  6 07:59:45 2020
+
+@author: raja
+"""
+
+
+import pandas as pd
+import numpy as np
+
+class Covid19Data:
+    dataUrl = []
+    fnames = []
+    countryNames = []
+    confDF = pd.DataFrame()
+    dtsDF = pd.DataFrame()
+    recoveredDF = pd.DataFrame()
+    
+    def __init__(self):
+        
+        self.dataUrl = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/"
+        self.fnames = ("time_series_covid19_confirmed_global.csv",
+                  "time_series_covid19_deaths_global.csv",
+                  "time_series_covid19_recovered_global.csv")
+                
+    def updateCountryNames(self):
+         
+        self.countryNames = self.confDF["Country/Region"].unique()
+        
+    def getCountryNameIndex(self, countryName="Canada"):
+         
+        return np.argmax(self.countryNames == "Canada")
+        
+         
+    def loadData(self):
+        
+        self.confDF = pd.read_csv(self.dataUrl + self.fnames[0])
+        self.dtsDF = pd.read_csv(self.dataUrl + self.fnames[1])
+        self.recoveredDF = pd.read_csv(self.dataUrl + self.fnames[2])
+        
+        self.updateCountryNames()
+        
+    
+        
+    def aggregateByCountry(inputDF):
+    
+        outputDF = pd.DataFrame(inputDF.groupby("Country/Region").agg("sum")).drop(columns=["Lat", "Long"]).transpose()
+        outputDF.index = pd.to_datetime(outputDF.index)
+        
+        return outputDF
+
+    def aggregateAllDataByCountryName(self,countryName="Canada"):
+        outputDF = pd.DataFrame(columns=["confirmed","deaths","recovered"])
+        outputDF["confirmed"] = pd.DataFrame(self.confDF.groupby("Country/Region").agg("sum")).drop(columns=["Lat","Long"]).transpose()[countryName]
+        outputDF["deaths"] = pd.DataFrame(self.dtsDF.groupby("Country/Region").agg("sum")).drop(columns=["Lat","Long"]).transpose()[countryName]
+        outputDF["recovered"] = pd.DataFrame(self.recoveredDF.groupby("Country/Region").agg("sum")).drop(columns=["Lat","Long"]).transpose()[countryName]
+        
+        #outputDF = pd.DataFrame(inputDFConf.groupby("Country/Region").agg("sum")).drop(columns=["Lat", "Long"]).transpose()
+        outputDF.index = pd.to_datetime(outputDF.index)
+        
+        return outputDF
+    
+
+# myData = Covid19Data()
+# myData.loadData()
+# print(myData.aggregateAllDataByCountryName())
